8516bcc2301b908131aeb01dd557fa59cbd0d02c
Svensemann93 <sven.landert@gmx.ch>
2026-07-30 14:41:35 +0200

    feat(profile): make tabs switchable and add the journals tab

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,21 +1,26 @@
+import { useState } from 'react'
 import AppHeader from '../components/AppHeader'
 import ProfileHero from '../components/ProfileHero'
 import ProfileStats from '../components/ProfileStats'
-import ProfileTabs from '../components/ProfileTabs'
+import ProfileTabs, { type ProfileTab } from '../components/ProfileTabs'
 import ProfileAbout from '../components/ProfileAbout'
+import ProfileJournals from '../components/ProfileJournals'
 
 function ProfilePage() {
+  const [tab, setTab] = useState<ProfileTab>('journals')
+
   return (
     <div className="min-h-screen bg-slate-50">
       <AppHeader />
 
       <main className="mx-auto max-w-7xl space-y-6 p-6 sm:p-8">
-        {' '}
         <ProfileHero />
         <ProfileStats />
+
         <div className="space-y-6">
-          <ProfileTabs active="about" />
-          <ProfileAbout />
+          <ProfileTabs active={tab} onSelect={setTab} />
+          {tab === 'journals' && <ProfileJournals />}
+          {tab === 'about' && <ProfileAbout />}
         </div>
       </main>
     </div>
